49c3093d2d4b6299c5b48f3f0af3f9bef8ad289d
dcweber <dcweber@us.ibm.com>
2020-04-13 09:40:19 -0500

    Update TestGetProfilesProfileId.java

--- modules/annotator-for-clinical-data/src/test/java/com/ibm/watson/health/acd/v1/profiles/TestGetProfilesProfileId.java
+++ modules/annotator-for-clinical-data/src/test/java/com/ibm/watson/health/acd/v1/profiles/TestGetProfilesProfileId.java
@@ -68,7 +68,7 @@ public class TestGetProfilesProfileId extends WatsonServiceTest {
 		for (Annotator annotator : annotators) {
 			Assert.assertNotNull(annotator.name());
 			Assert.assertNull(annotator.description());
-			Assert.assertNotNull(annotator.configurations());
+			Assert.assertNull(annotator.configurations());
 		}
 	}
 
@@ -86,7 +86,7 @@ public class TestGetProfilesProfileId extends WatsonServiceTest {
 		for (Annotator annotator : annotators) {
 			Assert.assertNotNull(annotator.name());
 			Assert.assertNull(annotator.description());
-			Assert.assertNull(annotator.configurations());
+			Assert.assertNotNull(annotator.configurations());
 		}
 	}
 
@@ -124,7 +124,7 @@ public class TestGetProfilesProfileId extends WatsonServiceTest {
 		for (Annotator annotator : annotators) {
 			Assert.assertNotNull(annotator.name());
 			Assert.assertNull(annotator.description());
-			Assert.assertNull(annotator.configurations());
+			Assert.assertNotNull(annotator.configurations());
 		}
 	}
 
