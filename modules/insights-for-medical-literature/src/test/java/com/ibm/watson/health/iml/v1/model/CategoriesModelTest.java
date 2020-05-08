78dda3535d08ba16484581e33cd8bdecdd84c28b
dcweber <dcweber@us.ibm.com>
2020-05-08 18:50:47 -0500

    Update CategoriesModelTest.java

--- modules/insights-for-medical-literature/src/test/java/com/ibm/watson/health/iml/v1/model/CategoriesModelTest.java
+++ modules/insights-for-medical-literature/src/test/java/com/ibm/watson/health/iml/v1/model/CategoriesModelTest.java
@@ -114,7 +114,7 @@ public class CategoriesModelTest {
     assertEquals(categoriesModelModel.highlightedTitle(), "testString");
     assertEquals(categoriesModelModel.highlightedAbstract(), "testString");
     assertEquals(categoriesModelModel.highlightedBody(), "testString");
-    assertEquals(categoriesModelModel.highlightedSections(), new java.util.HashMap<String,StringBuilder>(){{put("foo", "testString"); }});
+    assertEquals(categoriesModelModel.highlightedSections(), new java.util.HashMap<String,String>(){{put("foo", "testString"); }});
     assertEquals(categoriesModelModel.passages(), (new java.util.HashMap<String, PassagesModel>(){{put("foo", new PassagesModel(){{put("foo", new PassagesModel()); }}); }}));
     assertEquals(categoriesModelModel.annotations(), new java.util.HashMap<String,AnnotationModel>(){{put("foo", annotationModelModel); }});
 
