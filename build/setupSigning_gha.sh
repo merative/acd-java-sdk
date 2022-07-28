8625a01a2cf3d4634a2879f923580d2cdd9ee12c
Deb Angst <dlangst@merative.com>
2022-07-28 15:54:05 -0500

    fix: retry maven deploy

--- build/setupSigning_gha.sh
+++ build/setupSigning_gha.sh
@@ -2,7 +2,7 @@
 set -x
 
 echo "Importing signing key..."
-gpg --quiet --batch --yes --decrypt --passphrase="$SIGNING_PASSPHRASE" -- output ./build/signing.key ./build/signing.key.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$SIGNING_PASSPHRASE" --output ./build/signing.key ./build/signing.key.gpg
 
 gpg --version
 gpg --no-tty --batch --yes --import ./build/signing.key
